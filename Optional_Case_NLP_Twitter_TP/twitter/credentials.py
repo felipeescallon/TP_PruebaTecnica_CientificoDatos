30d854c69c34c2f2f5456bd3fdd9951236737f3b
Andres Felipe Escallon Portilla <aescallon@unicauca.edu.co>
2021-03-15 15:39:29 -0500

    Update credentials.py

--- Optional_Case_NLP_Twitter_TP/twitter/credentials.py
+++ Optional_Case_NLP_Twitter_TP/twitter/credentials.py
@@ -1,14 +1,7 @@
 #credentials.py
 
-#These are Andrés Felipe Escallón Portilla´s twitter developer keys:
-#API key: o3SeYJCGVh9FeAcBcZnFYjFRk
-#API secret key: tK4oicHzVF6QypyY6uyggTrjkDN14bA9BbiPikVy2Dyu6BkjUK
-#*Bearer token: AAAAAAAAAAAAAAAAAAAAADZsNQEAAAAAYA%2F2j7g%2FdVenRnh%2F1RAN4AcUuNk%3D1so6b7bkIxz8bTtJAZlRy0x1Rv4rZa9y1xLXJa6F2gGyehr9DF
-#Access token: 155321400-yuCpjnV21oAj7APVq7hSIbV1pS7lC2snP2ISH6yn
-#Access token secret: CF0AqRogayKocXhRfSiqBifUwR7XDoTnPYu9DoVtmTYB8
-
-API_KEY = 'o3SeYJCGVh9FeAcBcZnFYjFRk'
-API_SECRET_KEY = 'tK4oicHzVF6QypyY6uyggTrjkDN14bA9BbiPikVy2Dyu6BkjUK'
+API_KEY = 'xxxxxx'
+API_SECRET_KEY = 'xxxxxx'
 #*Bearer token(no needed here)
-ACCESS_TOKEN = '155321400-yuCpjnV21oAj7APVq7hSIbV1pS7lC2snP2ISH6yn'
-ACCESS_TOKEN_SECRET = 'CF0AqRogayKocXhRfSiqBifUwR7XDoTnPYu9DoVtmTYB8'
+ACCESS_TOKEN = 'xxxxxx'
+ACCESS_TOKEN_SECRET = 'xxxxxx'
